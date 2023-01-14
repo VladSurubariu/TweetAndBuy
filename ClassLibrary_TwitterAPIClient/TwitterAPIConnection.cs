9e95940f30b0241d3c69b1f320de8f0b5e5125c1
Vlad Surubariu <vladsurubariu@gmail.com>
2023-01-14 21:52:48 +0200

    Optimize the code

--- ClassLibrary_TwitterAPIClient/TwitterAPIConnection.cs
+++ ClassLibrary_TwitterAPIClient/TwitterAPIConnection.cs
@@ -12,17 +12,23 @@ namespace ClassLibrary_TwitterAPIClient
 {
     public class TwitterAPIConnection
     {
-        private static string consumer_key = "wNvgwbfz4rdx4o9lPXwv8jUTE";
-        private static string consumer_key_secret = "eHc6mooipwwSXlKvCluC61TrkZDWNPzWRRhYoyHM1baPXqVW9F";
-        private static string acces_token = "1358431620068425731-RwpIYNKT3Wh7Uc1whaRoEJxwgxz6PF";
-        private static string acces_token_secret = "f8pnZLsk08fx2DF6rOPdo6mz6PjJrcJkkZ7yvdK7Ze3IW";
+        private static string consumer_key;
+        private static string consumer_key_secret;
+        private static string acces_token;
+        private static string acces_token_secret;
         internal static string username = "vladsrb11";
+        List<ITweet> timelineTweets = new List<ITweet>();
+
+        public TwitterClient userClient;
+
+        public TwitterAPIConnection(string consumer_key, string consumer_key_secret, string acces_token, string acces_token_secret)
+        {
+            userClient = new TwitterClient(consumer_key, consumer_key_secret, acces_token, acces_token_secret);
+        }
 
         public async Task RetrieveTweets()
         {
-            var userClient = new TwitterClient(consumer_key, consumer_key_secret, acces_token, acces_token_secret);
             var timelineIterator = userClient.Timelines.GetUserTimelineIterator(username);
-            List<ITweet> timelineTweets = new List<ITweet>();
             int numberOfTweets = 0;
 
             while (!timelineIterator.Completed)
@@ -32,11 +38,13 @@ namespace ClassLibrary_TwitterAPIClient
                 numberOfTweets = Math.Max(page.Count(), numberOfTweets);
             }
 
+            /*
             for (var index = 0; index < numberOfTweets; index++)
             {
                 var tweet = timelineTweets[index];
                 Console.WriteLine(tweet.ToString());
             }
+            */
 
         }
     }
--- TweetAndBuy/Form1.cs
+++ TweetAndBuy/Form1.cs
@@ -18,5 +18,21 @@ namespace TweetAndBuy
             InitializeComponent();
         }
 
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string consumer_key = "wNvgwbfz4rdx4o9lPXwv8jUTE";
+            string consumer_key_secret = "eHc6mooipwwSXlKvCluC61TrkZDWNPzWRRhYoyHM1baPXqVW9F";
+            string acces_token = "1358431620068425731-RwpIYNKT3Wh7Uc1whaRoEJxwgxz6PF";
+            string acces_token_secret = "f8pnZLsk08fx2DF6rOPdo6mz6PjJrcJkkZ7yvdK7Ze3IW";
+
+            TwitterAPIConnection twitterConnection = new TwitterAPIConnection(consumer_key, consumer_key_secret, acces_token, acces_token_secret);
+            await twitterConnection.RetrieveTweets();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
